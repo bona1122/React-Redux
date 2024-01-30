32e0aca88f7c8ab16029f9e1f14243384703b497
bona1122 <vnclfjqm12@gmail.com>
2024-01-30 15:59:37 +0900

    Create a function component

--- react/2.2/src/app.js
+++ react/2.2/src/app.js
@@ -1,15 +1,23 @@
 /* @jsx createElement */
-import { createDOM, render, createElement } from "./react";
+import { render, createElement } from "./react";
 
-const vdom = (
+function Title(props) {
+  return <h1>{props.children}</h1>;
+}
+
+function Item(props) {
+  return <li style={`color:${props.color}`}>{props.children}</li>;
+}
+
+const App = () => (
   <p>
-    <h1>React 만들기</h1>
+    <Title>React 정말 잘 만들기</Title>
     <ul>
-      <li style="color: red">첫 번째 아이템</li>
-      <li style="color: blue">두 번째 아이템</li>
-      <li style="color: green">세 번째 아이템</li>
+      <Item color="red">첫 번째 아이템</Item>
+      <Item color="green">두 번째 아이템</Item>
+      <Item color="blue">세 번째 아이템</Item>
     </ul>
   </p>
 );
 
-render(vdom, document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
