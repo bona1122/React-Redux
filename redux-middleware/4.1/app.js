4a082eea3471eb88edf07cefa7b1ce9de1f4b502
bona1122 <vnclfjqm12@gmail.com>
2024-03-24 21:50:18 +0900

    change webpack-dev-middleware dependencies

--- redux-middleware/4.1/app.js
+++ redux-middleware/4.1/app.js
@@ -4,9 +4,30 @@ import reducer from "./reducer.js";
 import { SET_COUNTER, ASYNC_INCREASE_COUNTER } from "./action-type.js";
 import { logger } from "./logger.js";
 
-const store = createStore(reducer, [logger]);
+const asyncRouter = (jobs) => (store) => (next) => (action) => {
+  const matchJob = Object.entries(jobs).find(([type]) => action.type === type);
+  if (matchJob) {
+    matchJob[1](store, action);
+  } else {
+    next(action);
+  }
+};
+
+const asyncJobs = {
+  // 각 비동기 액션마다의 처리기
+  [ASYNC_INCREASE_COUNTER]: function (store, action) {
+    store.dispatch(Actions.asyncRequest());
+    setTimeout(() => {
+      store.dispatch(Actions.increase(20));
+      store.dispatch(Actions.asyncResponse());
+    }, 3000);
+  },
+};
+
+const store = createStore(reducer, [logger, asyncRouter(asyncJobs)]);
 
 const counterDisplay = document.querySelector("#counter");
+const loadingMessage = document.querySelector("#loading");
 const btnIncrease = document.querySelector("#btn-increase");
 const btnAsyncIncrease = document.querySelector("#btn-async-increase");
 const btnDecrease = document.querySelector("#btn-decrease");
@@ -14,8 +35,9 @@ const btnReset = document.querySelector("#btn-reset");
 
 store.subscribe(function () {
   // 상태가 바뀌면 실행될 함수
-  const { counter } = store.getState();
+  const { counter, request } = store.getState();
 
+  loadingMessage.style.visibility = request ? "visible" : "hidden";
   counterDisplay.textContent = counter;
 });
 
--- redux/3.3/reducer.js
+++ redux/3.3/reducer.js
@@ -10,8 +10,16 @@ export function reducer(state = InitializeState, action) {
     case ActionTypes.DECREASE:
       return { ...state, count: state.count - 1 };
     case ActionTypes.RESET:
-      return { ...state, count: 0 };
+      fetch("/reset")
+        .then((res) => res.json())
+        .then((result) => {
+          if (result) return { ...state, count: 0 };
+        });
+
     default:
       return { ...state };
   }
 }
+
+// reducer 함수는 순수 함수로 작성해야 한다.
+// 즉, 비동기 처리를 하면 안된다.
